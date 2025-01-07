428cc536b5fd9be9bd6728d797df17d4c70babb1
Valeriy <wiep1994#yandex.ru>
2025-01-07 10:17:42 +0300

    add folder

---
+++ my-library-app/src/main/java/com/example/my_library_app/MyLibraryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_library_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibraryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLibraryAppApplication.class, args);
+	}
+
+}
